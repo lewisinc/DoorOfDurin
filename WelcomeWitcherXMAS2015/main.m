17dafe751169131e13beea030f59a12ae9406a29
David Lewis <davidlewis@icloud.com>
2015-12-24 17:51:10 -0800

    Initial Commit

---
+++ WelcomeWitcherXMAS2015/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WelcomeWitcherXMAS2015
+//
+//  Created by David Lewis on 12/24/15.
+//  Copyright © 2015 David Lewis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
