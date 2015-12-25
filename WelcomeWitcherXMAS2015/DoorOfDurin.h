85704c1f4d4a8cba98fdb6cd123265469d474c18
David Lewis <davidlewis@icloud.com>
2015-12-25 12:26:38 -0800

    First actual commit.

---
+++ WelcomeWitcherXMAS2015/DoorOfDurin.h
@@ -0,0 +1,13 @@
+//
+//  DoorOfDurin.h
+//  WelcomeWitcherXMAS2015
+//
+//  Created by David Lewis on 12/24/15.
+//  Copyright © 2015 David Lewis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DoorOfDurin : NSObject
+
+@end
---
+++ WelcomeWitcherXMAS2015/DoorOfDurin.m
@@ -0,0 +1,13 @@
+//
+//  DoorOfDurin.m
+//  WelcomeWitcherXMAS2015
+//
+//  Created by David Lewis on 12/24/15.
+//  Copyright © 2015 David Lewis. All rights reserved.
+//
+
+#import "DoorOfDurin.h"
+
+@implementation DoorOfDurin
+
+@end
